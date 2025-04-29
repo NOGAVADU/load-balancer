571d9f07a58a5894b8e24e32ca516334934cf850
Daniil Mosolov <dkuets@gmail.com>
2025-04-29 20:07:09 +0300

    feat: add sever starting

--- cmd/load_balancer/main.go
+++ cmd/load_balancer/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nogavadu/load_balancer/internal/config"
 	"github.com/nogavadu/load_balancer/internal/lib/logger/sl"
 	"github.com/nogavadu/load_balancer/pkg/pretty_slog"
 	"log/slog"
+	"net/http"
 	"os"
 )
 
@@ -21,10 +23,19 @@ func main() {
 		logger.Error("failed to load config", sl.Err(err))
 		os.Exit(1)
 	}
-	logger.Info("config initialized")
-
 	if len(cfg.BackendsPool) == 0 {
 		logger.Error("empty backends pool")
 		os.Exit(1)
+	}
+	logger.Info("config initialized")
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.HTTPServer.Port),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+	}
+
+	logger.Info("load balancer started", slog.Int("port", cfg.HTTPServer.Port))
+	if err = server.ListenAndServe(); err != nil {
+		logger.Error("failed to start server", sl.Err(err))
 	}
 }
